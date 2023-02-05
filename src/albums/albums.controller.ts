88e9d42726540fce6bc227ee89b4b6156c1bbde8
Mark Karnaukh <marik13021992@gmail.com>
2023-02-05 14:07:37 +0100

    Define basic endpoints for 'albums'

--- src/albums/albums.controller.ts
+++ src/albums/albums.controller.ts
@@ -1,7 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { AlbumsService } from './albums.service';
 
 @Controller('album')
 export class AlbumsController {
   constructor(private readonly albumsService: AlbumsService) {}
+
+  @Get()
+  findAll() {
+    return 'find all albums';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `find one album by id ${id}`;
+  }
+
+  @Post()
+  create(@Body() createAlbumDto: unknown) {
+    return `create new album: ${JSON.stringify(createAlbumDto)}`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateAlbumDto: unknown) {
+    return `create album ${id}: ${JSON.stringify(updateAlbumDto)}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `remove album ${id}`;
+  }
 }
