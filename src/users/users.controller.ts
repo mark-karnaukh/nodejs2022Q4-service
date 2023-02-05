797b9d25bf059dbe5286795b145792d3134000c6
Mark Karnaukh <marik13021992@gmail.com>
2023-02-05 15:10:48 +0100

    Define basic endpoints for 'users'

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('user')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findAll() {
+    return 'find all users';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `find one user by id ${id}`;
+  }
+
+  @Post()
+  create(@Body() createUserDto: unknown) {
+    return `create new user: ${JSON.stringify(createUserDto)}`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: unknown) {
+    return `update user ${id}: ${JSON.stringify(updateUserDto)}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `remove user ${id}`;
+  }
+}
