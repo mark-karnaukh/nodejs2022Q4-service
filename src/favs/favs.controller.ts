f64f3f535b25af3f6d4b4f1de2837613b95478dc
Mark Karnaukh <marik13021992@gmail.com>
2023-02-05 15:35:30 +0100

    Define basic endpoints for 'favorites'

--- src/favs/favs.controller.ts
+++ src/favs/favs.controller.ts
@@ -1,7 +1,50 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { AlbumsService } from 'src/albums/albums.service';
+import { ArtistsService } from 'src/artists/artists.service';
+import { TracksService } from 'src/tracks/tracks.service';
 import { FavsService } from './favs.service';
 
 @Controller('favs')
 export class FavsController {
-  constructor(private readonly favsService: FavsService) {}
+  constructor(
+    private readonly favsService: FavsService,
+    private readonly tracksService: TracksService,
+    private readonly albumsService: AlbumsService,
+    private readonly artistsService: ArtistsService,
+  ) {}
+
+  @Get()
+  getAll() {
+    return 'get all favorites';
+  }
+
+  @Post('/track/:id')
+  addTrack(@Param('id') id: string) {
+    return `add track ${id} to the favorites`;
+  }
+
+  @Delete('/track/:id')
+  removeTrack(@Param('id') id: string) {
+    return `remove track ${id} from favorite`;
+  }
+
+  @Post('/album/:id')
+  addAlbum(@Param('id') id: string) {
+    return `add album ${id} to the favorites`;
+  }
+
+  @Delete('/album/:id')
+  removeAlbum(@Param('id') id: string) {
+    return `remove album ${id} from favorite`;
+  }
+
+  @Post('/artist/:id')
+  addArtist(@Param('id') id: string) {
+    return `add artist ${id} to the favorites`;
+  }
+
+  @Delete('/artist/:id')
+  removeArtist(@Param('id') id: string) {
+    return `remove artist ${id} from favorite`;
+  }
 }
--- src/favs/favs.module.ts
+++ src/favs/favs.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AlbumsService } from 'src/albums/albums.service';
+import { ArtistsService } from 'src/artists/artists.service';
+import { TracksService } from 'src/tracks/tracks.service';
 import { FavsController } from './favs.controller';
 import { FavsService } from './favs.service';
 
 @Module({
   controllers: [FavsController],
-  providers: [FavsService],
+  providers: [FavsService, TracksService, AlbumsService, ArtistsService],
 })
 export class FavsModule {}
